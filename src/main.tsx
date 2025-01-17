3048db1a097143cfcfde815cdd8543afdbf970ba
William <william.deazevedopro@gmail.com>
2025-01-17 15:20:20 +0100

    debut react router

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import App from "./App";
+import "./index.css";
+import Root from "./routes/root";
+import SignIn from "./routes/sign-in";
+import User from "./routes/user";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<App />}>
+          <Route index element={<Root />} />
+          <Route index path="sign-in" element={<SignIn />} />
+          <Route index path="user" element={<User />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
